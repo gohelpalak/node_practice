44e1881d06fbc3750525a78fba856d8dea672eba
palak <gohelpalak14@gmail.com>
2024-12-17 14:34:29 +0530

    pr delete

--- pr/node_practice/bookstore-app/config/config.js
+++
@@ -1,7 +0,0 @@
-const mongoose= require("mongoose")
-const dbConnect=()=>{
-    mongoose.connect("mongodb://localhost:27017/")
-    .then(() => console.log('DB Connected!!'))
-    .catch((err) => console.log(err));
-}
-module.exports = dbConnect();
